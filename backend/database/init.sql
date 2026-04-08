d648da68dfaae15c9e897c95dee240f91f62e23b
OscarYavar <oscaryavarxd@gmail.com>
2026-04-08 17:40:38 +0100

    Added biodata table to init.sql

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -4,3 +4,16 @@ CREATE TABLE app.public.users (
   email VARCHAR(255) UNIQUE NOT NULL,
   pasaword VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE app.public.biodata (
+    id SERIAL PRIMARY KEY,
+    genre VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    height INT NOT NULL,
+    goal VARCHAR(255) NOT NULL,
+    activity VARCHAR(255) NOT NULL,
+    c_weight INT NOT NULL,
+    d_weight INT NOT NULL,
+    weeks INT NOT NULL
+);
+
