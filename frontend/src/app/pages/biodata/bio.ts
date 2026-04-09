6b81887a3f1c5951f49205d048d903896f18fc9e
OscarYavar <oscaryavarxd@gmail.com>
2026-04-09 21:37:30 +0100

    Finished bioData form!

--- frontend/src/app/pages/biodata/bio.ts
+++ frontend/src/app/pages/biodata/bio.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bio',
@@ -13,6 +14,13 @@ export class bio {
   height: number | undefined;
   genre: string = ``;
   protected bioData: number = 0;
+  goal: string = '';
+  act: string = '';
+  cKg: number | undefined;
+  dKg: number | undefined;
+  nWeeks: number | undefined;
+
+  constructor(private router: Router) {}
 
   changeBio() {
     this.bioData += 1;
@@ -24,5 +32,24 @@ export class bio {
     sessionStorage.setItem('age', this.age.valueOf());
     // @ts-ignore
     sessionStorage.setItem('height', this.height.valueOf());
+    sessionStorage.setItem('goal', this.goal.valueOf());
+    sessionStorage.setItem('act', this.act.valueOf());
+    // @ts-ignore
+    sessionStorage.setItem('cKg', this.cKg.valueOf());
+    // @ts-ignore
+    sessionStorage.setItem('dKg', this.dKg.valueOf());
+    // @ts-ignore
+    sessionStorage.setItem('nWeeks', this.nWeeks.valueOf());
+    this.router.navigate(['/login']);
+  }
+
+  onChange(e: any) {
+    if (e.target.name === 'radio') {
+      this.goal = e.target.value;
+    }
+
+    if (e.target.name === 'radio1') {
+      this.act = e.target.value;
+    }
   }
 }
