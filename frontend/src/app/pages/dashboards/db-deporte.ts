35d6aedf0d8c07bad4b88bcefc2d2c878cc74b16
Margarita <cruzperez.marga@gmail.com>
2026-04-24 18:12:57 +0100

    Added dashboard component html and css

---
+++ frontend/src/app/pages/dashboards/db-deporte.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-db-dieta',
+  templateUrl: './db-dieta.html',
+  styleUrls: ['./db-dieta.css']
+})
+
+export class DbDeporteComponent {
+
+}
