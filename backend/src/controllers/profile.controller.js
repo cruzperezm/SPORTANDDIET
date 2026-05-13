a9952fa09f7b538df52698952ae18f4d0da8b654
SarTastica <SarTastica@>
2026-05-13 17:25:03 +0100

    fix: getProfile added in  profile.controller

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -1,5 +1,32 @@
 const prisma = require('../config/prisma');
 
+const getProfile = async (req, res) => {
+  try {
+    const userId = 1;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        biometrics: true
+      }
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error al obtener el perfil:", error);
+    res.status(500).json({ error: "Error al obtener datos del servidor" });
+  }
+};
+
+module.exports = {
+  updateProfile,
+  getProfile
+};
+
 const updateProfile = async (req, res) => {
   try {
     const userId = 1;
--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { updateProfile } = require('../controllers/profile.controller');
 
 router.put('/profile', updateProfile);
+router.get('/profile', getProfile);
 
 module.exports = router;
